2ffc4ceb98fd17c89b8a92046ee7471b19d6eb89
Lokesh Chandramurthy <79828815+MechieTech7@users.noreply.github.com>
2021-08-24 11:18:36 +0530

    Update code-Remote html.java

--- code-Remote html.java
+++ code-Remote html.java
@@ -1,1 +1,47 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class TextFile {
 
+	public static void main(String[] args) {
+		
+		int numLines = 0;
+		int numWords = 0;
+		int numChars = 0;
+		
+		Scanner myFile = null;
+		try {
+			Scanner file = new Scanner(new File ("file:///C:/Users/lokesh.chandramurthy/Documents/1.html"));
+			myFile = file;
+			
+			
+		} catch (FileNotFoundException ioException) {
+			System.out.println("Your file does not exist.");
+		}
+		while(myFile.hasNextLine()) {
+			String curLine = myFile.nextLine();
+			int size = curLine.length();
+			boolean foundDiv = true ;
+			boolean foundChar = false ;
+			for (int LineSize = 0; LineSize < size ; LineSize++) {
+				if(curLine.charAt(LineSize) == ' ') {
+					foundDiv = true;
+					foundChar = false;
+				
+					}else {
+						foundChar = true;
+						numChars++ ;
+					}
+			if(foundChar && foundDiv) {
+				numWords++;
+				foundDiv = false;
+			}
+		}
+			numLines++;
+	}
+		myFile.close();
+		System.out.println("Number of Words = " + numWords);
+		System.out.println("Number of Lines = " + numLines);
+		System.out.println("Number of Char =  " + numChars);
+}
+}
